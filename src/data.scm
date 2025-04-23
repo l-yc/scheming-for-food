756d397f14d2890a0a62b508dceef55338cab243
Felix Prasanna <fpx@mit.edu>
2025-04-23 11:53:21 -0400

    init + initial data modelling

---
+++ src/data.scm
@@ -0,0 +1,82 @@
+(load "src/utils.scm")
+
+(define-record-type <ingredient>
+  (make-ingredient name)
+  ingredient?
+  (name ingredient-name))
+
+(define-print-method ingredient?
+  (standard-print-method
+   "ingredient"
+   (lambda (i)
+     `(("name" ,(ingredient-name i))))))
+
+(define-record-type <quantity>
+  (%make-quantity amount unit)
+  quantity?
+  (amount quantity-amount)
+  (unit quantity-unit))
+
+(define (make-quantity amount unit)
+  (assert (unit? unit) "make-quantity: received invalid unit")
+  (%make-quantity amount unit))
+
+(define-print-method
+  quantity?
+  (standard-print-method
+   "quantity"
+   (lambda (q)
+     `(("amount" ,(quantity-amount q))
+       ("unit" ,(quantity-unit q))))))
+
+;; Units are represented as symbols
+(define (unit? thing)
+  (or (in? '(tsp tbsp floz cup pt qt gal ml l) thing) ;; Volume
+      (in? '(oz lb g kg) thing)                       ;; Weight
+      (in? '(F C) thing)))                            ;; Temperature
+
+;; Given a unit, returns the canonical unit that should be used in place of it.
+(define (canonical-unit unit)
+  (case unit
+    ((tsp tbsp floz cup pt qt gal ml l) 'ml)
+    ((oz lb g kg) 'g)
+    ((F C) 'C)
+    (else (error "canonical-unit: unknown unit" unit))))
+
+;; Given a unit, returns a procedure that can be used to take a amount in
+;; that unit and transform it into an amount in the canonical unit for the
+;; given unit.
+;; Example: given tsp, returns a procedure that multiplies the input by
+;; 4.928922, as this is the conversion ratio from tsp -> mL, the caconical unit
+;; for volume.
+(define (conversion-to-canonical unit)
+  (define (times-by factor)
+    (lambda (x) (* x factor)))
+  (case unit
+    ;; Volume: canonical unit mL
+    ((tsp)  (times-by 4.928922))
+    ((tbsp) (times-by 14.78677))
+    ((floz) (times-by 29.57353))
+    ((cup)  (times-by 236.5882))
+    ((pt)   (times-by 473.1765))
+    ((qt)   (times-by 946.353))
+    ((gal)  (times-by 3785.412))
+    ((ml)   (times-by 1.))
+    ((l)    (times-by 1000.))
+    ;; Weight: canonical unit g
+    ((oz)   (times-by 28.34952))
+    ((lb)   (times-by 453.5924))
+    ((g)    (times-by 1.))
+    ((kg)   (times-by 1000.))
+    ;; Tempererature: canonical unit C
+    ((F)    (lambda (f) (/ (* 5. (- f 32.)) 9.)))
+    ((C)    (times-by 1.))
+    (else   (error "conversion-to-canonical: unknown unit" unit))))
+
+;; Converts a quantity into an equivalent quantity in canonical units
+;; TODO: should this automatically be called in make-quantity?
+(define (canonicalize-quantity q)
+  (let ((unit (quantity-unit q)) (amount (quantity-amount q)))
+    (make-quantity
+      ((conversion-to-canonical unit) amount)
+      (canonical-unit unit))))
---
+++ src/utils.scm
@@ -0,0 +1,3 @@
+;; Check if x is a member of l
+(define (in? l x)
+  (if (memv x l) #t #f))
